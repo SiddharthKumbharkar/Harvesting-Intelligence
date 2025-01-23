46ff1113cfd2247600f258f7acbfe1f41fa75a92
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-01-23 09:55:04 +0530

    harshita commit

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -1,6 +1,11 @@
 console.log("welcome")
 let signin = document.getElementById('signInLink');
+let disease = document.getElementById('feature1');
 
 signin.addEventListener('click', () => {
     window.location.href = 'login.html';
+})
+
+disease.addEventListener('click', () => {
+    window.location.href = 'disease_detection.html';
 })
