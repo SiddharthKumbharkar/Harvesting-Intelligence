767b22d6e6013343a91856b65185f98368253214
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-01-29 20:08:17 +0530

    new commits

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -1,21 +1,6 @@
-console.log("welcome")
-let signin = document.getElementById('signInLink');
-let disease = document.getElementById('feature1');
-let yieldOp = document.getElementById('feature2');
-let cropSelection = document.getElementById('feature3');
+console.log('Harvesting Intelligence website loaded.');
+let signIn = document.getElementById('signin');
 
-signin.addEventListener('click', () => {
-    window.location.href = 'login.html';
-})
-
-disease.addEventListener('click', () => {
-    window.location.href = 'disease_detection.html';
-})
-
-yieldOp.addEventListener('click', () => {
-    window.location.href = 'yield_optimization.html';
-})
-
-cropSelection.addEventListener('click', () => {
-    window.location.href = 'crop_selection.html';
+signIn.addEventListener('click', () => {
+    window.location.href = 'sign-in.html';
 })
