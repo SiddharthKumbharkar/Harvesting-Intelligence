cef3cca437f75686c58e6cb29e51d8430d65c36e
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-02-05 10:41:06 +0530

    new clone commits

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -1,6 +1,21 @@
 console.log('Harvesting Intelligence website loaded.');
 let signIn = document.getElementById('signin');
+let disease = document.getElementById('disease-detection');
+let cropselect = document.getElementById('crop-selection');
+let yieldOp = document.getElementById('yield-optimisation');
 
 signIn.addEventListener('click', () => {
-    window.location.href = 'sign-in.html';
+    window.location.href = 'login.html';
+})
+
+disease.addEventListener('click', () => {
+    window.location.href = 'disease_detection.html';
+})
+
+yieldOp.addEventListener('click', () => {
+    window.location.href = 'yield_optimisation.html';
+})
+
+cropselect.addEventListener('click', () => {
+    window.location.href = 'crop_selection.html';
 })
