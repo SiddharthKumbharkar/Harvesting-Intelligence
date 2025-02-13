afe94743df989c41c714345ab8574b652df0e885
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-02-13 10:34:26 +0530

    new cloned

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -1,19 +1,24 @@
 console.log('Harvesting Intelligence website loaded.');
 let signIn = document.getElementById('signin');
-let disease = document.getElementById('disease-detection');
 let cropselect = document.getElementById('crop-selection');
 let yieldOp = document.getElementById('yield-optimisation');
 
-// Redirecting Pages
+// Sign In
 signIn.addEventListener('click', () => {
     window.location.href = 'login.html';
 })
-disease.addEventListener('click', () => {
-    window.location.href = 'disease_detection.html';
-})
+
+// Yield optimisation
 yieldOp.addEventListener('click', () => {
     window.location.href = 'yield_optimisation.html';
 })
+
+// Crop Selection
 cropselect.addEventListener('click', () => {
     window.location.href = 'crop_selection.html';
 })
+
+// Disease detection
+function selectCrop(cropName){
+    window.location.href =  `index_${cropName}.html`;
+}
