5859add36f9ccc9f39811ad87affb5552e770770
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-02-13 08:52:20 +0530

    changes

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -4,18 +4,16 @@ let disease = document.getElementById('disease-detection');
 let cropselect = document.getElementById('crop-selection');
 let yieldOp = document.getElementById('yield-optimisation');
 
+// Redirecting Pages
 signIn.addEventListener('click', () => {
     window.location.href = 'login.html';
 })
-
 disease.addEventListener('click', () => {
     window.location.href = 'disease_detection.html';
 })
-
 yieldOp.addEventListener('click', () => {
     window.location.href = 'yield_optimisation.html';
 })
-
 cropselect.addEventListener('click', () => {
     window.location.href = 'crop_selection.html';
 })
