d8be868e250906b3b859b957a0c25876d7234969
Sanavi Kulkarni <sanaviskul@gmail.com>
2025-03-06 09:30:15 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 // app.use((req, res) => {
 //     res.status(404).json({ error: 'Route not found' });
 //   });
-  
+
 //   const PORT = process.env.PORT || 5000;
 //   app.listen(PORT, () => {
 //     console.log(`Server running on port ${PORT}`);
