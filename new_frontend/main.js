7a1a5457a56b3c1817eca3f1ee550b7aa0c18128
Harshita-211105 <pathrabeharshita@gmail.com>
2025-03-19 10:44:56 +0530

    model companion

--- new_frontend/main.js
+++ new_frontend/main.js
@@ -15,7 +15,7 @@ yieldOp.addEventListener('click', () => {
 
 // Crop Selection
 cropselect.addEventListener('click', function (){
-    window.location.href = "crop_selection.html";
+    window.location.href = "http://127.0.0.1:3000/frontend/companion.html";
 })
 
 // Disease detection
