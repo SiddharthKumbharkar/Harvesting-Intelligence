4d4f9281aa1d50ed48d352049857fc558f928aa1
Harshita-211105 <pathrabeharshita@gmail.com>
2025-03-27 09:56:59 +0530

    port changes

--- backend/index.js
+++ backend/index.js
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5173;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
