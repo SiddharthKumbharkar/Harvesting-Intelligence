2cd1290f66fcd3a5eff70fe49e6b89ef858f8254
Sanavi Kulkarni <sanaviskul@gmail.com>
2025-04-02 10:39:32 +0530

    Final commit

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ require('dotenv').config();
 
 const app = express();
 // app.use(cors());
-app.use(cors({ origin: "http://127.0.0.1:5500", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 app.use('/auth', require('./routes/authRoutes'));
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
